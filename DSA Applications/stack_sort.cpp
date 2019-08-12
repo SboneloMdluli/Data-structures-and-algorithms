8c51a238657c5df19b1007558cfd69b9c367a6f2
SboneloMdluli <1101772@students.wits.ac.za>
2019-08-12 16:18:35 +0200

    sort a stack in ascending order

---
+++ DSA Applications/stack_sort.cpp
@@ -0,0 +1,55 @@
+//Algorithm to sort a stack in ascending order
+
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+void sort_stack (stack<int> &num){
+
+     stack<int> sorted; // sorted stack
+     
+     //O(n^2)
+     while(!num.empty()){
+        
+        int temp = num.top(); // get top element
+        num.pop();
+        
+    
+        while(!sorted.empty() && sorted.top() > temp){
+            
+            num.push(sorted.top());
+            sorted.pop();
+        
+       }
+        
+        sorted.push(temp);
+     
+     }
+     
+    // return sorted;
+    while(!sorted.empty()){
+        
+        cout << sorted.top() <<endl; // get top element
+        sorted.pop();
+        
+    }
+}
+
+
+int main(){
+
+      stack<int> num ;
+      num.push(6);
+      num.push(3);
+      num.push(1);
+      num.push(4);
+      num.push(7);
+      num.push(2);
+      
+  //    stack<int> sorted ;
+     sort_stack(num);
+      
+  
+}
+
