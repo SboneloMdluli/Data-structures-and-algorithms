1eb85dc28ffcfa565edf062ff777869e6295029f
SboneloMdluli <1101772@students.wits.ac.za>
2019-08-12 15:04:21 +0200

    string is one edit distance

---
+++ DSA Applications/oneDistanceEdit.cpp
@@ -0,0 +1,54 @@
+//algorithm to check if string is 1 edit distance away
+
+#include<iostream>
+#include<string>
+#include<cstdlib>
+
+using namespace std;
+
+bool is_edit_1(string s1, string s2)
+{
+	int m = s1.length();
+	int n = s2.length();
+	int visited[256] ={0}; //O(1) space
+	
+	if (abs(m-n) > 1)
+	     return false;
+	else
+	{
+	    for (int i =0;i<m;i++){
+		visited[s1[i]]++;
+	    }
+
+	}
+	
+	// O(n) running time
+
+	for (int i =0;i<n;i++)
+	{
+		visited[s2[i]]--;
+	}
+
+	// O(1)
+	int count =0;
+	for (int i =0;i<256;i++)
+	{
+		if(visited[i] !=0)
+			count++;
+	}
+	
+	return count==1;
+}
+
+int main(){
+
+     string a = "once";
+     string b = "one";
+
+     if(is_edit_1(a,b)){
+        cout <<"one distance edit"<<endl;
+     }else{
+        cout <<"false"<<endl;
+     }
+
+}
