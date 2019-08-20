76adc47c08b718a60d4779328562ae160e10268e
SboneloMdluli <1101772@students.wits.ac.za>
2019-08-20 16:31:48 +0200

    inorder traversal

--- Binary Search Tree/bst.cpp
+++ Binary Search Tree/bst.cpp
@@ -4,20 +4,23 @@ using namespace std;
 
 template <typename T>
 class node {
-        
+    
+    public:
     node(){
-        left = NULL;
-        right = NULL;
+        node<T>* left = NULL;
+        node<T>* right = NULL;
     }
 
     T elem; // store data 
     node<T>* left;  
     node<T>* right;
+    
 };
 
 template <typename T>
 class bst{
         public:
+          bst();
           int const size();
           bool isEmpty() const;
           void add(const T& elem);
@@ -25,14 +28,15 @@ class bst{
           node<T> *root;
           
         private:
-          node<T> * add(node<T> *root,const T& key);
-          int size;
+          node<T> *add(node<T> *root,const T& key);
+          void inorder(node<T> *newNode);
+          int Size;
 };
 
 template <typename T>
 bst<T>::bst(){
         root = NULL;
-        size = 0;
+        Size = 0;
 }
 
 template <typename T>
@@ -42,44 +46,62 @@ bool bst<T>::isEmpty() const{
 
 template <typename T>
 int const bst<T>::size(){
-        return size;
+        return Size;
 }
-
 
 template <typename T>
 void bst<T> :: add(const T& key){
-    //O(1)
-       // Otherwise add this element to the binary tree
+ 
     {
       root = add(root, key);
-      size++;
+      Size++;
     }
 }
 
 template <typename T>
-node<T> *bst<T>:: add(node<T> *root,const T& key){
+node<T> *bst<T>:: add(node<T> *newNode,const T& key){
    // make root
-    if (node == null) {
+    if (newNode == NULL) {
       node<T> *newNode = new node<T>; // create new node 
       newNode-> elem = key;
-
-    } /* Otherwise, recur down the tree */
+      
+      return newNode;
+    } 
     
-    if (key < node->key) 
-        node->left  = add(node->left, key); 
-    else if (key > node->key) 
-        node->right = add(node->right, key);   
-
-    return node;
+    if (key < newNode->elem) 
+        newNode->left  = add(newNode->left, key); 
+    else if (key > newNode->elem) 
+        newNode->right = add(newNode->right, key);   
+     
+    return newNode;
 }
+
+template <typename T>
+void bst<T>::inorder(node<T> *newNode) 
+{ 
+    if (newNode != NULL) 
+    { 
+       inorder(newNode->left); 
+       cout << newNode->elem<<endl; 
+       inorder(newNode->right); 
+    } 
+} 
+
 
 template <typename T>
 void bst<T>::inorder() 
 { 
-    if (*root != NULL) 
-    { 
-        inorder(root->left); 
-        cout << root->key<<endl; 
-        inorder(root->right); 
-    } 
+    inorder(root); 
 } 
+
+
+int main(){
+
+    bst<int> FirstTree;
+    FirstTree.add(3);
+    FirstTree.add(2); 
+    FirstTree.add(1);
+    FirstTree.add(4);
+       
+    FirstTree.inorder();
+}
