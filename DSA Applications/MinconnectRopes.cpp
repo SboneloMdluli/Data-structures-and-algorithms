a09659c217b7cce7b8e0c7370801f21b6587fb48
SboneloMdluli <1101772@students.wits.ac.za>
2019-12-06 14:36:53 +0200

    min cost to connect ropes

---
+++ DSA Applications/MinconnectRopes.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int minCost(vector<int>& num){
+    
+    if (num.size() ==  0)
+        return 0;
+        
+    priority_queue< int, vector<int>, greater<int> > p_q(num.begin(), num.end());
+    
+    int res, sum=0;
+    
+    while (p_queue.size() > 1)  {
+        res = 0;
+        res += p_q.top(); 
+        p_q.pop();
+
+        res += p_q.top(); 
+        p_q.pop();
+
+        p_q.push(res);
+        
+        sum +=res;
+    }
+    return sum;
+}
+
+
+int main(){
+
+      vector<int> num = {2, 2, 3, 3};
+      cout<< minCost(num) <<endl;
+}
