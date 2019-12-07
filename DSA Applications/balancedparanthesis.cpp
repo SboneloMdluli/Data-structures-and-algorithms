b0cfcfad99a4954743d0313eedb8146afbb2d76c
SboneloMdluli <1101772@students.wits.ac.za>
2019-12-07 16:00:41 +0200

    2D binary search

--- DSA Applications/balancedparanthesis.cpp
+++ DSA Applications/balancedparanthesis.cpp
@@ -1,27 +1,29 @@
-//Program to check if an expression contains balanced paranthesis
+// check if string has balanced parenthesis
 
 #include <stack>
 #include <unordered_map>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-bool balanced_par(char str[],int &size){
+bool balanced_par(string& str){
 
-       //O(n) space
+       if(str.size()==0){
+	   return true;
+       }
+
        stack <char> lookup;
        unordered_map <char,char> complement_prthns;
        
-       //O(1) space
        complement_prthns.insert(make_pair('}','{'));
        complement_prthns.insert(make_pair(')','('));
        complement_prthns.insert(make_pair(']','['));
        
-       //O(n) runtime
        if(str[0] == '}' || str[0] == ')' || str[0]==']')
             return false;
 
-       for(int i=0; i <size; i++){
+       for(int i=0; i <	str.size() ; i++){
         
           if(str[i] == '{' || str[i] == '(' || str[i]=='[' ){
                          
@@ -51,12 +53,11 @@ bool balanced_par(char str[],int &size){
 
 int main(){
 
-        char str[] = "[{{}}(){}]";
-        int size = sizeof(str)/sizeof(str[0]);
+        string str = "[{{}}(){}]";
         
-        if (balanced_par(str,size))
+        if (balanced_par(str))
                 cout <<"balanced"<<endl;
-        else 
+        else
                cout << "not balanced"<<endl;
-        
 }
+
---
+++ DSA Applications/gridSearch.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool grid_search(vector<vector<int>>&num, int k){
+        
+        if(num.size()==0)
+              return false;
+        
+        int left = 0;
+        int rows = num.size();
+        int cols = num[0].size();
+        int right = rows*cols-1;
+        int mid;
+        
+        while(right>=left){
+        
+           mid = left + (right-left)/2;
+           
+           if(num[mid/cols][mid%cols]==k){
+                return true;
+           }
+           
+           if(num[mid/cols][mid%cols]>k){
+              right = mid-1;
+           }else{
+              left = mid+1;
+           }
+        
+        }
+        
+        return false;
+
+}
+
+int main(){
+
+        vector<vector<int>> grid = {
+        
+                {1,3,5,6},
+                {7,8,9,10},
+                {12,14,20,21}
+        
+        };
+        
+        
+        if(grid_search(grid,14)){
+            cout <<"found"<<endl;
+        }
+
+}
